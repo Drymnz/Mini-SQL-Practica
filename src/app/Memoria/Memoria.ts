ed5746e9acb186f2b40a239d4e6ed11e08abec8b
Benjamín de Jesús Pérez Aguilar <mcdreck276@gmail.com>
2023-04-05 19:05:29 -0600

    termine la asgnacion con valores

--- src/app/Memoria/Memoria.ts
+++ src/app/Memoria/Memoria.ts
@@ -1,90 +1,100 @@
 // para analizar
 import { Token } from "src/app/database/Token"
-import { Tabla,Atributo,TipoDato } from "src/app/database/Tabla"
+import { Tabla, Atributo, TipoDato } from "src/app/database/Tabla"
 import { Valor } from "../database/Valor";
-import { Opereaciones , TipoOperacion } from "../database/Opereaciones";
+import { Opereaciones, TipoOperacion } from "../database/Opereaciones";
 import { Asignacion } from "../database/Asignacion";
 import { ElementoTabla } from "../database/ElementoTabla";
 import { Imprimir } from "../database/Imprimir";
 import { Declaracion } from "../database/Declaracion";
 import { Set } from "../database/Set";
 import { Consulta } from "../database/Consulta";
-import { Filtro , TipoFiltro} from "../database/Filtro";
-import { InstruccionIF , InstruccionELSE, InstruccionELSEIF } from "../database/InstruccionIF";
+import { Filtro, TipoFiltro } from "../database/Filtro";
+import { InstruccionIF, InstruccionELSE, InstruccionELSEIF } from "../database/InstruccionIF";
 // para mantener
 import { TablaEjecucion } from "./TablaEjecucion";
 import { Variable } from "../database/Variable";
 
 export class Memoria {
 
-  tablas:Array<TablaEjecucion> = new Array;
-  private list:Array<TablaEjecucion> = [];
-  private listVariables:Array<Variable> = [];
+  tablas: Array<TablaEjecucion> = new Array;
+  private list: Array<TablaEjecucion> = [];
+  private listVariables: Array<Variable> = [];
 
   cargar(realizar: Token[]) {
-    if (realizar != undefined && realizar.length>0) {
-      this.listVariables = [] ;
+    if (realizar != undefined && realizar.length > 0) {
+      this.listVariables = [];
       realizar.forEach(element => {
         if (element instanceof Tabla && this.insertTabla(element)) {
           this.tablas.push(new TablaEjecucion(element));
         }
         if (element instanceof ElementoTabla && this.insertElementoTabla(element)) {
           this.list[0].addElemento(element);
-        } 
-        if(element instanceof Declaracion){
+        }
+        if (element instanceof Declaracion) {
           this.insertarVariable(element);
         }
-        if(element instanceof Set){
-          
+        if (element instanceof Set) {
+          this.asignacionValoresVariables(element);
         }
       });
     }
   }
 
-  insertTabla(element:Tabla):Boolean {
+  insertTabla(element: Tabla): Boolean {
     if (this.tablas.length > 0) {
-       return !(this.tablas.filter(p=>p.tablas.name == element.name).length>0);
+      return !(this.tablas.filter(p => p.tablas.name == element.name).length > 0);
     }
     return true;
   }
 
-  insertElementoTabla(element:ElementoTabla):Boolean {
+  insertElementoTabla(element: ElementoTabla): Boolean {
     if (this.tablas.length > 0) {
       this.list = this.tablas.filter(//TablaEjecucion memoria
-        p1=>p1.tablas.listadoAtributo.filter(//Tabla memoria
-        p2=>//Listado Atributo de tabla memoria
-         element.listadoAtributos.filter(
-          p3 =>p3.nombre == p2.name // este atributo tiene el mismo nombre
-          ).length == 1// tiene la misma cantidad de atributos 
-          ).length == p1.tablas.listadoAtributo.length
-          //Tabla
-          //TablaEjecucion
-          );
-      return (this.list.length==1);
+        p1 => p1.tablas.listadoAtributo.filter(//Tabla memoria
+          p2 =>//Listado Atributo de tabla memoria
+            element.listadoAtributos.filter(
+              p3 => p3.nombre == p2.name // este atributo tiene el mismo nombre
+            ).length == 1// tiene la misma cantidad de atributos 
+        ).length == p1.tablas.listadoAtributo.length
+        //Tabla
+        //TablaEjecucion
+      );
+      return (this.list.length == 1);
     }
     return true;
   }
 
 
-  insertarVariable(element:Declaracion){
+  insertarVariable(element: Declaracion) {
     const linea = element.line;
     const column = element.column;
     const valorVariable = element.valor;
     const tipoVariable = element.tipo;
-    if (this.listVariables.length<1) {
+    if (this.listVariables.length < 1) {
       element.nombre.forEach(itmString => {
-        this.listVariables.push(new Variable(linea,column,itmString,tipoVariable,valorVariable));
+        this.listVariables.push(new Variable(linea, column, itmString, tipoVariable, valorVariable));
       });
     } else {
-      var nombre;
-      var boolenInsert = true;
-      const ver:Declaracion = element;
-      var listadoInserVariable = element.nombre.filter(p=> this.listVariables.filter(p2 => p2.nombre == p).length == 0 );
-      if (listadoInserVariable.length>0) {
+      const ver: Declaracion = element;
+      var listadoInserVariable = element.nombre.filter(p => this.listVariables.filter(p2 => p2.nombre == p).length == 0);
+      if (listadoInserVariable.length > 0) {
         listadoInserVariable.forEach(itmString => {
-          this.listVariables.push(new Variable(linea,column,itmString,tipoVariable,valorVariable));
+          this.listVariables.push(new Variable(linea, column, itmString, tipoVariable, valorVariable));
         });
       }
+    }
+  }
+
+  asignacionValoresVariables(element: Set) {
+    if (this.listVariables.length > 0) {
+      const itimEle: Set = element;
+      this.listVariables.forEach(element_uno => {
+        const listAsig = itimEle.listadoAsignacion.filter(p => p.nombre == element_uno.nombre);
+        listAsig.forEach(element => {
+          element_uno.valor = element.getValor();
+        });
+      });
     }
   }
 }
--- src/app/database/Asignacion.ts
+++ src/app/database/Asignacion.ts
@@ -5,27 +5,29 @@ import { Opereaciones } from "./Opereaciones";
 export class Asignacion extends Token{
 
     nombre: any;
-    private valor:Token | undefined;
+    private valor:Token | Valor | undefined;
 
     constructor(
         line: number,
         column: number,
         nombre: any,
-        valor:Token | undefined
+        valor:Token | Valor | undefined
       ) {
         super(line, column);
         this.nombre = nombre ;
         this.valor = valor;
       }
 
-     getValor()   {
+     getValor():Valor | undefined  {
       if(this.valor instanceof Valor){
-        this.valor;
+        return this.valor;
       }else{
         if (this.valor instanceof Opereaciones) {
-          const convertir:Opereaciones = this.valor;
+          const convertir:Opereaciones = this.valor as Opereaciones;
           this.valor = convertir.getValue();
         }
+        return this.valor as Valor;
       }
+      return undefined;
     }
 }
